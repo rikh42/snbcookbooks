0c84d5d47e0af18c3652922b3160613f230692c6
Rik Heywood <rik@smallneatbox.co.uk>
2011-10-04 15:46:37 +0100

    ...

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -4,8 +4,8 @@
 
 Chef::Log.warn("#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo")
 
-hg "/home/site/checkouts/www" do
-  repository "https://#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
-  reference "tip"
-  action :sync
-end
+#hg "/home/site/checkouts/www" do
+#  repository "https://#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
+#  reference "tip"
+#  action :sync
+#end
