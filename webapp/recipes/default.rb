5fc6a2967ab215f203a030c659686a460560fa12
Rik Heywood <rik@smallneatbox.co.uk>
2011-10-04 16:10:55 +0100

    ...

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -2,11 +2,9 @@
 ## testing deploying an app with mercurial
 
 
-Chef::Log.warn("Hello")
-Chef::Log.warn("#{node[:snb][:kilnuser]}:#{node[:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo")
-
-#hg "/home/site/checkouts/www" do
-#  repository "https://#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
-#  reference "tip"
-#  action :sync
-#end
+Chef::Log.warn("Syncing site from Kiln")
+hg "/home/site/checkouts/www" do
+  repository "https://#{node[:snb][:kilnuser]}:#{node[:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
+  reference "tip"
+  action :sync
+end
