ffc1d2e937ab32b02a3e67d2556601422b2f3531
Rik Heywood <rik@smallneatbox.co.uk>
2011-10-04 14:55:12 +0100

    used proper kiln repo

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -2,7 +2,7 @@
 ## testing deploying an app with mercurial
 
 hg "/home/site/checkouts/www" do
-  repository "https://bitbucket.org/zurmo/zurmo"
+  repository "https://cded8771-cc27-49dd-85b6-f3131e20dc05:none@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
   reference "tip"
   action :sync
 end
