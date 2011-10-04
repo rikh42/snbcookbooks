0ceed72e6d694b0492ed3f6b22ac840df59ff81f
Rik Heywood <rik@smallneatbox.co.uk>
2011-10-04 15:44:25 +0100

    ...

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -1,6 +1,9 @@
 
 ## testing deploying an app with mercurial
 
+
+Chef::Log.warn("#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo")
+
 hg "/home/site/checkouts/www" do
   repository "https://#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
   reference "tip"
