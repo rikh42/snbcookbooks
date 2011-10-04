f3bd6df9660a671dba52a35d1cfb9bca6b741964
Rik Heywood <rik@smallneatbox.co.uk>
2011-10-04 15:26:09 +0100

    changed to using custom json

--- webapp/recipes/default.rb
+++ webapp/recipes/default.rb
@@ -2,7 +2,7 @@
 ## testing deploying an app with mercurial
 
 hg "/home/site/checkouts/www" do
-  repository "https://cded8771-cc27-49dd-85b6-f3131e20dc05:none@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
+  repository "https://#{node[:deploy][:snb][:kilnuser]}:#{node[:deploy][:snb][:kilnpass]}@smallneatbox.kilnhg.com/Repo/Repositories/CMS/promo"
   reference "tip"
   action :sync
 end
